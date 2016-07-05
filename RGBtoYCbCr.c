a0875deaccc72bae4dba8b69364168d2fded8467
4444jw4444 <jorinweatherston@gmail.com>
2016-07-05 14:56:06 -0700

    Removed unecessary checks from command line checker

--- RGBtoYCbCr.c
+++ RGBtoYCbCr.c
@@ -6,20 +6,9 @@
 int commandLineArgsValid(int argc, char** argv){
 	//check that the correct number of command line arguments were passed in 
 	if(argc != 3){
-		printf( "Wrong number of command line arguments\n"); 
+		printf( "ERROR:Wrong number of command line arguments\n"); 
 		return 0;
 	}
-	/*//check that the second command line argument is a valid input WAV audio file
-	std::string wav = ".wav";
-	if(!(((std::string)argv[1]).find(wav) != std::string::npos)){
-		std::cerr << "Incorrect input filename format. Must end with .wav\n";
-		return false;
-	}
-	//check that the third command line argument is a valid output WAV audio file
-	if(!(((std::string)argv[2]).find(wav) != std::string::npos)){
-		std::cerr << "Incorrect output filename format. Must end with .wav\n";
-		return false;
-	}*/
 	  
 	return 1;
 }
