2ddc6472674f78c9956bcac854553f839a99b523
Jordan Heemskerk <jordanh@shaw.ca>
2016-07-05 15:19:51 -0700

    Sample all 4 pixels to convert

--- RGBtoYCbCr.c
+++ RGBtoYCbCr.c
@@ -30,6 +30,9 @@ int main(int argc, char** argv){
     uint32_t src1[SRC_WIDTH];
     uint32_t src2[SRC_WIDTH];        
 
+    uint32_t dest1[SRC_WIDTH/2];
+    uint32_t dest2[SRC_WIDTH/2];
+
     FILE *src_file;
 
     src_file = fopen(argv[1], "rb");
@@ -39,7 +42,7 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    float r,g,b,y,cr,cb;
+    float r,g,b,y1,y2,y3,y4,cr,cb;
     uint32_t rgba;
 
     int i,j;
@@ -56,15 +59,45 @@ int main(int argc, char** argv){
             g = (float)(uint8_t)(rgba >> 16);
             b = (float)(uint8_t)(rgba >> 8);
 
-            printf("(%f, %f, %f)", r, g, b);
-
-            y = 16.0 + 0.257*r + 0.504*g + 0.098*b;
+            y1 = 16.0 + 0.257*r + 0.504*g + 0.098*b;
             cb = 128.0 - 0.148*r - 0.291*g + 0.439*b;
             cr = 128.0 + 0.439*r - 0.368*g - 0.071*b;
 
-            printf(" --> (%f, %f, %f)\n", y, cb, cr);
+            rgba = src1[j+1];
+            
+            r = (float)(uint8_t)(rgba >> 24);
+            g = (float)(uint8_t)(rgba >> 16);
+            b = (float)(uint8_t)(rgba >> 8);
 
-    
+            y2 = 16.0 + 0.257*r + 0.504*g + 0.098*b;
+            cb += 128.0 - 0.148*r - 0.291*g + 0.439*b;
+            cr += 128.0 + 0.439*r - 0.368*g - 0.071*b;
+            
+            rgba = src2[j];
+            
+            r = (float)(uint8_t)(rgba >> 24);
+            g = (float)(uint8_t)(rgba >> 16);
+            b = (float)(uint8_t)(rgba >> 8);
+
+            y3 = 16.0 + 0.257*r + 0.504*g + 0.098*b;
+            cb += 128.0 - 0.148*r - 0.291*g + 0.439*b;
+            cr += 128.0 + 0.439*r - 0.368*g - 0.071*b;
+            
+            rgba = src2[j+1];
+            
+            r = (float)(uint8_t)(rgba >> 24);
+            g = (float)(uint8_t)(rgba >> 16);
+            b = (float)(uint8_t)(rgba >> 8);
+
+            y4 = 16.0 + 0.257*r + 0.504*g + 0.098*b;
+            cb += 128.0 - 0.148*r - 0.291*g + 0.439*b;
+            cr += 128.0 + 0.439*r - 0.368*g - 0.071*b;
+
+            cb /= 4;
+            cr /= 4;
+
+            //YCrY and YCbY into 2 uint32_t's and store in dest1 and dest2 at j/2
+
         }
 
     }
